737c8ca13091195314eb47623c529cf3026f34de
myokoyama <part7steelballrun.to.8@gmail.com>
2025-04-21 18:29:48 +0900

    fix: 🐛 Markdown のフロントマターの型付けの方法をドキュメントにあったものに修正する

--- src/scripts/types.ts
+++ src/scripts/types.ts
@@ -1,13 +1,11 @@
 export type MarkdownBlogPost = {
-  frontmatter: {
-    title: string;
-    pubDate: Date;
-    updatedAt?: Date;
-    description: string;
-    image?: {
-      url: string;
-      alt: string;
-    };
-    tags: string[];
+  title: string;
+  pubDate: Date;
+  updatedAt?: Date;
+  description: string;
+  image?: {
+    url: string;
+    alt: string;
   };
+  tags: string[];
 };
