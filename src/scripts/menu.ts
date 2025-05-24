273f6e44286fb54a96310c09d5ce0b02124a0214
myokoyama <part7steelballrun.to.8@gmail.com>
2025-05-24 23:35:07 +0900

    fix: 🐛 開閉時でハンバーガーアイコンを切り替える

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -1,5 +1,16 @@
 document.querySelector('.hamburger')?.addEventListener('click', () => {
-  document
-    .querySelector('.mobile-nav .nav-links')
-    ?.classList.toggle('expanded');
+  const navLinks = document.querySelector('.mobile-nav .nav-links');
+  const hamburgerIcon = document.getElementById('hamburgerIcon');
+
+  // メニューの展開状態を切り替え
+  navLinks?.classList.toggle('expanded');
+
+  // アイコンの切り替え
+  if (hamburgerIcon?.classList.contains('fa-bars')) {
+    hamburgerIcon.classList.remove('fa-bars');
+    hamburgerIcon.classList.add('fa-times'); // バッテンアイコン
+  } else {
+    hamburgerIcon?.classList.remove('fa-times');
+    hamburgerIcon?.classList.add('fa-bars'); // ハンバーガーアイコン
+  }
 });
